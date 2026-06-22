d230de1412e76c6a3fa5566ecf6e70714018acde
Artur Khelshtein <artur.khelshtein@live.com>
2026-06-23 01:44:05 +0300

    fix: css

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,7 +3,7 @@ import { useTranslations } from 'next-intl';
 export default function HomePage() {
   const t = useTranslations('HomePage');
   return (
-    <main style={{ textAlign: 'center', padding: '40px' }}>
+    <main>
       <h1>{t('title')}</h1>
       <p>test_test</p>
     </main>
